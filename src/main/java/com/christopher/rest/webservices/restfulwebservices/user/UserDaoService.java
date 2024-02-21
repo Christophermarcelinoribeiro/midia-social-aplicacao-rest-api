f738d31656b2145273c107832fdbcff7fa696953
Christophermarcelinoribeiro <gui.amapa66@gmail.com>
2024-02-21 09:13:17 -0300

    Created exception class and functionality to show the id of the created user

--- src/main/java/com/christopher/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/christopher/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -1,33 +1,53 @@
 package com.christopher.rest.webservices.restfulwebservices.user;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
+
+
+
+
 
 @Component
 public class UserDaoService {
 	private static List<User> users= new ArrayList<>();
-	
+	private static int usersCount =0;
 	static {
-		users.add(new User(1,"naruto",LocalDate.now().minusYears(30)));
-		users.add(new User(2,"boruto",LocalDate.now().minusYears(10)));
-		users.add(new User(3,"kakashi",LocalDate.now().minusYears(20)));
+		users.add(new User(++usersCount,"naruto",LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount,"boruto",LocalDate.now().minusYears(10)));
+		users.add(new User(++usersCount,"kakashi",LocalDate.now().minusYears(20)));
 	}
 	
 	public List<User> findAll(){
 		return users;
 	}
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
 
 
-	public User findById(int id) {
+	public User findById(int id) throws Exception {
 		for(User u: users) {
 			if(u.getId().equals(id)) {
 				return u;
 			}
 		}
-		return null;
+		throw new UserNotFoundException("id" +id );
+		
+		
+		
 	}
+/*	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().get();
+	}
+*/
+		
 }
--- src/main/java/com/christopher/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/christopher/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,11 +1,15 @@
 package com.christopher.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 
@@ -28,6 +32,14 @@ public class UserResource {
 	public User retrieveAllUsers(@PathVariable int id) throws Exception{
 		return service.findById(id);
 	}
-	
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		//mostra o local onde foi criado o usuario. Ex: http://localhost:8080/users/6
+		URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
 
 }
