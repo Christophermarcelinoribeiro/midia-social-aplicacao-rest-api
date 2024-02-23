ff93cdc475fe9487832659a47de5357d1b5ded92
Christophermarcelinoribeiro <gui.amapa66@gmail.com>
2024-02-23 00:21:21 -0300

    Created internationalization - i18n

--- src/main/java/com/christopher/rest/webservices/restfulwebservices/helloword/HelloWordController.java
+++ src/main/java/com/christopher/rest/webservices/restfulwebservices/helloword/HelloWordController.java
@@ -1,5 +1,9 @@
 package com.christopher.rest.webservices.restfulwebservices.helloword;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,7 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWordController {
+	private MessageSource messagesource;
 
+	public HelloWordController(MessageSource messagesource) {
+		this.messagesource = messagesource;
+	}
 	@GetMapping(path = "/hello-word")
 	public String helloWord() {
 		return "hellow word";
@@ -19,5 +27,11 @@ public class HelloWordController {
 	@GetMapping(path = "/hello-word/path variable/{name}")
 	public HelloWordBean helloWordBean(@PathVariable String name) {
 		return new HelloWordBean(String.format("hello word, %s", name));
+	}
+	@GetMapping(path = "/hello-word-internationalized")
+	public String helloWordInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messagesource.getMessage("good.morning.message", null,"Default message", locale);
+		//return "hellow word";
 	}
 }
